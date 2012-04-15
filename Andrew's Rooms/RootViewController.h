cbeb39603b6704136c37b0f91d97b8f90d141f49
Andrew Luke <asluke@gmail.com>
2012-04-14 23:01:46 -0700

    Initial Commit

---
+++ Andrew's Rooms/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Andrew's Rooms
+//
+//  Created by Andrew Luke on 4/14/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
---
+++ Andrew's Rooms/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Andrew's Rooms
+//
+//  Created by Andrew Luke on 4/14/12.
+//  Copyright __MyCompanyName__ 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
